8c48648c891405704cb12eff6be52cf61370688a
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-04-06 07:34:21 +1000

    pexpert/arm64: strip BCM2712 of BCM2837 defines

--- pexpert/pexpert/arm64/BCM2712.h
+++ pexpert/pexpert/arm64/BCM2712.h
@@ -1,10 +1,44 @@
-// this is messy, because i just created a new file within XNU, is it thereby forced as APSL or can I license this as BSD-3?
+/*
+ * Copyright (c) 2025 Zormeister. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef _PEXPERT_ARM_BCM2712_H
 #define _PEXPERT_ARM_BCM2712_H
 
 /* and so it begins, hell. */
 
-#include "BCM2837.h"
+#ifdef BCM2712
+#include "arm64_common.h"
+#endif
+
+#define NO_MONITOR 1
+#define NO_ECORE 1
+
+/* TODO: Use PL011 or should I use the AUX UART I/O? */
 
 #endif
