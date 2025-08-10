c43d4e3c7bde18501442eaabc84940671ead1029
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-08-10 14:06:11 +1000

    lldbmacros: i am going crazy
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- tools/lldbmacros/core/syntax_checker.py
+++ tools/lldbmacros/core/syntax_checker.py
@@ -13,7 +13,7 @@ import sys
 import os
 import re
 
-tabs_search_rex = re.compile("[^\s]*\t+",re.MULTILINE|re.DOTALL)
+tabs_search_rex = re.compile("^\\s*\\t+",re.MULTILINE|re.DOTALL)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
