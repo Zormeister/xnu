b554510c16e6f7edcb19d995065a29caaec25aeb
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-08-21 20:58:57 +1000

    preflight: I can't write bash scripts apparently.
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- ci_preflight.sh
+++ ci_preflight.sh
@@ -2,7 +2,7 @@
 
 # Handle libplatform here
 
-if [ -f $PWD/depconfig/libplatform ] then
+if [ -f $PWD/depconfig/libplatform ]; then
 
 git clone --branch $(cat $PWD/depconfig/libplatform) https://github.com/apple-oss-distributions/libplatform.git ~/libplatform
 mkdir -p $(xcrun --sdk macosx -show-sdk-path)/usr/local/include/os/internal
